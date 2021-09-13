c24de6abd56102f29c68b393ac409a88fc351816
Mariana Cris <marianacris.ifal@gmail.com>
2021-09-13 19:54:10 +0000

    mudancas comentario revisor

--- calc-api/src/main/java/app/Soma.java
+++ calc-api/src/main/java/app/Soma.java
@@ -9,6 +9,7 @@ public class Soma{
     public String rotaSoma(@PathParam double op1, @PathParam double op2){
         try{
             return String.format("%.2f", op1+op2);
+            // adicionando mudanças comentário revisor!!!
         }catch(NumberFormatException nfe){
             throw new BadRequestException(String.format("Valores inválidos"));
         }
